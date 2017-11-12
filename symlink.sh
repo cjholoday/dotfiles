9ab13a09975e42473d59c52bb2ea33fec3a0914e
Colton Holoday <choloday@umich.edu>
2017-11-12 11:17:58 -0500

    Remove machine id logic from symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-# Usage: sh setup.sh [force] [machine_id]
-#
-# [machine_id] will be written to a file named machine_id in the dotfiles directory. It
-# is used to identify which machine this installation is for. This is useful for 
-# determining which PS1 to use, for example
+# Usage: sh setup.sh [force]
 #
 # If any string is passed for the force argument, then files in $HOME will be overwritten
 # with symlinks
 force="$1"
-machine_id="$2"
 
 
 try_symlink() {
@@ -36,10 +31,6 @@ dotfiles_path="$HOME/dotfiles"
 if [ ! -d "$dotfiles_path" ] || [ ! -f "$dotfiles_path/setup.sh" ]; then
     printf 'error: the dotfiles repository must be placed in $HOME\n'
     exit 1
-fi
-
-if [ ! -z "$machine_id" ]; then
-    echo "$machine_id" > "$dotfiles_path"/machine_id
 fi
 
 # symlink all the dotfiles into $HOME

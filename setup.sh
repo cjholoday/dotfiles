639b33a7067dc3df9300121a9b954ffb22036efe
Colton Holoday <>
2025-11-12 22:56:15 -0500

    Add visibility to git setup

--- setup.sh
+++ setup.sh
@@ -13,7 +13,9 @@ fi
 echo
 
 printf "Setting up git profile...\n"
-if [ ! -f derived/gitconfig_profile ]; then
+if [ -f derived/gitconfig_profile ]; then
+    printf "Already set up: $(realpath derived/gitconfig_profile)\n"
+else
     printf "Enter your full name: "
     read fullname
     printf "[user]\n    name = $fullname\n" > derived/gitconfig_profile
